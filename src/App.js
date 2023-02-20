3db22e3da33748fbb9cfa6257b390bf330e6eaa6
amontero1211 <110789222+amontero1211@users.noreply.github.com>
2023-02-19 23:31:25 -0400

    Actualizacion rutas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
 import Forgotpassword from './routes/Forgotpassword';
-import Home from './routes/Home';
+import Landing from './routes/Landing';
 import Login from './routes/Login';
 import Profile from './routes/Profile';
 import Subject from './routes/Subject';
 import Unit from './routes/Unit';
 import Admin from './routes/Admin';
+import Home from './routes/Landing';
 
 function App() {
   return ( 
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Landing />} />
       <Route path="Forgotpassword" element={<Forgotpassword />} />
       <Route path="Login" element={<Login />} />
       <Route path="Profile" element={<Profile />} />
       <Route path="Subject" element={<Subject />} />
       <Route path="Unit" element={<Unit />} />
       <Route path="Admin" element={<Admin />} />
-    </Routes>
+      <Route path="Home" element={<Home />}  />
+    </ Routes>
   );
 }
 
---
+++ src/routes/Landing.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Landing() {
+  return (
+    <div>
+      <Link to="/Login">
+        <h3>Login</h3>
+      </Link>
+      ;
+    </div>
+  );
+}
+
+export default Landing;
