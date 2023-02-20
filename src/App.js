67d934270f93644aa93fc91f3af27924f0061291
amontero1211 <110789222+amontero1211@users.noreply.github.com>
2023-02-19 22:58:27 -0400

    Adicion de rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Forgotpassword from './routes/Forgotpassword';
+import Home from './routes/Home';
+import Login from './routes/Login';
+import Profile from './routes/Profile';
+import Subject from './routes/Subject';
+import Unit from './routes/Unit';
+import Admin from './routes/Admin';
+
 function App() {
   return ( 
-    <div>
-      <h1>Hola mundo</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="Forgotpassword" element={<Forgotpassword />} />
+      <Route path="Login" element={<Login />} />
+      <Route path="Profile" element={<Profile />} />
+      <Route path="Subject" element={<Subject />} />
+      <Route path="Unit" element={<Unit />} />
+      <Route path="Admin" element={<Admin />} />
+    </Routes>
   );
 }
 
 export default App;
-d
